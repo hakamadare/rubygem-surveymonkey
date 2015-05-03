cbc745a1d1e57bcc5c158a69beb419b12e807f6d
Steve Huff <shuff@vecna.org>
2015-05-03 12:51:46 -0400

    make an object hierarchy
    trying to break my Perl habits of doing everything in giant data
    structures

--- lib/surveymonkey/client.rb
+++ lib/surveymonkey/client.rb
@@ -9,91 +9,15 @@ class Surveymonkey::Client
   $log.debug("Defined Surveymonkey::Client.")
 
   # constants
-  Baseuri = 'https://api.surveymonkey.net'
-
-  Api_methods = {
-    'create_flow' => {
-      'path'   => '/v2/batch/create_flow',
-    },
-    'send_flow' => {
-      'path'   => '/v2/batch/send_flow',
-    },
-    'create_collector' => {
-      'path'   => '/v2/collectors/create_collector',
-    },
-    'get_survey_list' => {
-      'path'   => '/v2/surveys/get_survey_list',
-    },
-    'get_survey_details' => {
-      'path'   => '/v2/surveys/get_survey_details',
-    },
-    'get_collector_list' => {
-      'path'   => '/v2/surveys/get_collector_list',
-    },
-    'get_respondent_list' => {
-      'path'   => '/v2/surveys/get_respondent_list',
-    },
-    'get_responses' => {
-      'path'   => '/v2/surveys/get_responses',
-    },
-    'get_response_counts' => {
-      'path'   => '/v2/surveys/get_response_counts',
-    },
-    'get_template_list' => {
-      'path'   => '/v2/templates/get_template_list',
-    },
-    'get_user_details' => {
-      'path'   => '/v2/user/get_user_details',
-    },
-  }
 
   # public methods
-  attr_reader :baseuri, :api_key
-  attr_accessor :access_token
+  attr_reader :baseuri, :api_key, :access_token
 
-  def api_call(api_method, method_params = {}, api_key = self.api_key, access_token = self.access_token)
+  def initialize(baseuri, access_token, api_key)
     begin
-      $log.debug(sprintf("%s: calling '%s'\n", __method__, api_method))
-
-      the_api_method = _api_method(api_method)
-
-      body        = _api_method_params(method_params)
-
-      path        = the_api_method.fetch('path')
-
-      http_method = the_api_method.fetch('method', 'post')
-
-      $log.debug(sprintf("%s: %s '%s' '%s'\n", __method__, http_method, path, body))
-
-      http_headers = _http_headers(access_token)
-      $log.debug(sprintf("%s: http_headers: '%s'\n", __method__, http_headers.inspect))
-
-      request_uri = _request_uri(path, api_key)
-
-      $log.debug(sprintf("%s: ready to make request for '%s'\n", __method__, api_method))
-      response = self.class.send(http_method.to_sym, request_uri, body: body, headers: http_headers)
-
-      $log.debug(sprintf("%s: response class %s\n", __method__, response.class))
-      $log.debug(sprintf("%s: response code %i\n", __method__, response.code))
-      $log.debug(sprintf("%s: response headers '%s'\n", __method__, response.headers.inspect))
-
-      response.parsed_response
-
-    rescue KeyError => e
-      $log.error(sprintf("%s: no such method '%s': %s\n", __method__, http_method, e.message))
-      raise e
-    rescue Exception => e
-      $log.error(sprintf("%s: %s\n", __method__, e.message))
-      raise
-    end
-  end
-
-  def initialize(*args)
-    begin
-      param_hash = args.shift || {}
-      @baseuri      = param_hash.fetch('baseuri', Baseuri)
-      @access_token = param_hash.fetch('access_token', _from_env('SURVEYMONKEY_ACCESSTOKEN'))
-      @api_key      = param_hash.fetch('api_key', _from_env('SURVEYMONKEY_APIKEY'))
+      @baseuri      = baseuri
+      @access_token = access_token
+      @api_key      = api_key
 
       self.class.logger $log, :debug
 
@@ -101,12 +25,10 @@ class Surveymonkey::Client
       self.class.base_uri @baseuri
 
       $log.debug(sprintf("%s: setting headers'\n", __method__))
-      the_headers = {
-        "Content-Type"  => "application/json",
-        "Authorization" => sprintf("bearer %s", @access_token),
-      }
-      self.class.headers the_headers
-    rescue Exception => e
+      http_headers = _http_headers(@access_token)
+      self.class.headers http_headers
+
+    rescue StandardError => e
       $log.error(sprintf("%s: %s\n", __method__, e.message))
       raise
     end
@@ -114,22 +36,6 @@ class Surveymonkey::Client
 
   # private methods
   private
-
-  def _api_method(key, api_methods = Api_methods)
-    begin
-      $log.debug(sprintf("%s: fetching '%s' from api methods\n", __method__, key))
-      value = api_methods.fetch(key)
-      $log.debug(sprintf("%s: retrieved '%s'\n", __method__, value.inspect))
-      value
-
-    rescue KeyError => e
-      $log.error(sprintf("%s: '%s' not found in api methods\n", __method__, key))
-      raise e
-    rescue Exception => e
-      $log.error(sprintf("%s: %s\n", __method__, e.message))
-      raise
-    end
-  end
 
   def _http_headers(token)
     begin
@@ -141,49 +47,7 @@ class Surveymonkey::Client
       $log.debug(sprintf("%s: http headers: '%s'\n", __method__, http_headers))
       http_headers
 
-    rescue Exception => e
-      $log.error(sprintf("%s: %s\n", __method__, e.message))
-      raise
-    end
-  end
-
-  def _api_method_params(method_params)
-    begin
-      # TODO validate params against API spec
-      $log.debug(sprintf("%s: parsing api method params from '%s'\n", __method__, method_params))
-      the_params = JSON.generate(method_params || {}).to_s
-      $log.debug(sprintf("%s: parsed method params '%s'\n", __method__, the_params))
-      the_params
-
-    rescue Exception => e
-      $log.error(sprintf("%s: %s\n", __method__, e.message))
-      raise
-    end
-  end
-
-  def _request_uri(path, api_key)
-    begin
-      $log.debug(sprintf("%s: generating request uri fragment from '%s' and '%s'\n", __method__, path, api_key))
-      request_uri = sprintf("%s?api_key=%s", path, api_key)
-      $log.debug(sprintf("%s: generated '%s'\n", __method__, request_uri))
-      request_uri
-
-    rescue Exception => e
-      $log.error(sprintf("%s: %s\n", __method__, e.message))
-      raise
-    end
-  end
-
-  def _from_env(key)
-    begin
-      $log.debug(sprintf("%s: fetching '%s' from environment\n", __method__, key))
-      value = ENV.fetch(key)
-      $log.debug(sprintf("%s: retrieved '%s'\n", __method__, value))
-      value
-
-    rescue KeyError => e
-      $log.info(sprintf("%s: '%s' not found in environment\n", __method__, key))
-    rescue Exception => e
+    rescue StandardError => e
       $log.error(sprintf("%s: %s\n", __method__, e.message))
       raise
     end
---
+++ lib/surveymonkey/request.rb
@@ -0,0 +1,142 @@
+require 'surveymonkey/logging'
+require 'surveymonkey/api'
+require 'surveymonkey/client'
+
+class Surveymonkey::Request
+
+  begin
+    attr_reader :api, :api_method, :client, :path, :api_key, :baseuri
+    attr_accessor :access_token, :method_params
+
+    # constants
+
+    Baseuri = 'https://api.surveymonkey.net'
+
+    # public methods
+    def execute(method_params = self.method_params, api_method = self.api_method, api_key = self.api_key, access_token = self.access_token)
+      begin
+        $log.debug(sprintf("%s: enter\n", __method__))
+
+        self.method_params=(self.api.api_method_params(method_params))
+        method_params = self.method_params
+        $log.debug(sprintf("%s: method_params: %s\n", __method__, method_params))
+
+        path = api_method.path
+        $log.debug(sprintf("%s: path: %s\n", __method__, path))
+
+        http_method = api_method.http_method
+        $log.debug(sprintf("%s: http_method: %s\n", __method__, http_method))
+
+        request_uri = _request_uri(path, api_key)
+        $log.debug(sprintf("%s: ready to make request for '%s'\n", __method__, api_method))
+
+        response = self.client.class.send(http_method.to_sym, request_uri, body: self.method_params)
+
+        $log.debug(sprintf("%s: response class %s\n", __method__, response.class))
+        $log.debug(sprintf("%s: response code %i\n", __method__, response.code))
+        $log.debug(sprintf("%s: response headers '%s'\n", __method__, response.headers.inspect))
+
+        response.parsed_response
+
+      rescue StandardError => e
+        $log.error(sprintf("%s: unable to execute API request: %s\n", __method__, e.message))
+        raise
+      end
+    end
+
+    def initialize(api_method, *args)
+      begin
+        $log.debug(sprintf("%s: enter\n", __method__))
+        $log.debug(sprintf("%s: api_method: %s\n", __method__, api_method))
+        $log.debug(sprintf("%s: args: %s\n", __method__, args))
+
+        api = Surveymonkey::API.new
+        @api_method = api.api_method(api_method)
+
+        # extract optional params
+        param_hash = Hash(args.shift) || {}
+        @baseuri       = param_hash.fetch('baseuri', Baseuri)
+        @method_params = api.api_method_params(param_hash.fetch('method_params', {}))
+        @access_token  = param_hash.fetch('access_token', _from_env('SURVEYMONKEY_ACCESSTOKEN'))
+        @api_key       = param_hash.fetch('api_key', _from_env('SURVEYMONKEY_APIKEY'))
+
+        # configure the client
+        @client = Surveymonkey::Client.new(baseuri = @baseuri, access_token = @access_token, api_key = @api_key)
+
+        # configure the API
+        @api = Surveymonkey::API.new
+
+      rescue StandardError => e
+        $log.error(sprintf("%s: unable to initialize API request: %s\n", __method__, e.message))
+        raise
+      end
+    end
+
+    # private methods
+    private
+
+    def _client
+      begin
+        @client = Surveymonkey::Client.new()
+      rescue StandardError => e
+        $log.fatal(sprintf("%s: %s\n", "Unable to initialize REST client", e.message))
+        $log.debug(sprintf("%s: %s\n", __method__, e.message))
+        raise
+      end
+    end
+
+    def _api
+      begin
+        @api = Surveymonkey::API.new()
+      rescue StandardError => e
+        $log.fatal(sprintf("%s: %s\n", "Unable to initialize SurveyMonkey API", e.message))
+        $log.debug(sprintf("%s: %s\n", __method__, e.message))
+        raise
+      end
+    end
+
+    def _http_headers(token)
+      begin
+        $log.debug(sprintf("%s: constructing http headers with token '%s'\n", __method__, token))
+        http_headers = {
+          "Content-Type" => "application/json",
+          "Authorization" => sprintf("bearer %s", token),
+        }
+        $log.debug(sprintf("%s: http headers: '%s'\n", __method__, http_headers))
+        http_headers
+
+      rescue Exception => e
+        $log.error(sprintf("%s: %s\n", __method__, e.message))
+        raise
+      end
+    end
+
+    def _from_env(key)
+      begin
+        $log.debug(sprintf("%s: fetching '%s' from environment\n", __method__, key))
+        value = ENV.fetch(key)
+        $log.debug(sprintf("%s: retrieved '%s'\n", __method__, value))
+        value
+
+      rescue KeyError => e
+        $log.info(sprintf("%s: '%s' not found in environment\n", __method__, key))
+      rescue Exception => e
+        $log.error(sprintf("%s: %s\n", __method__, e.message))
+        raise
+      end
+    end
+
+    def _request_uri(path, api_key)
+      begin
+        $log.debug(sprintf("%s: generating request uri fragment from '%s' and '%s'\n", __method__, path, api_key))
+        request_uri = sprintf("%s?api_key=%s", path, api_key)
+        $log.debug(sprintf("%s: generated '%s'\n", __method__, request_uri))
+        request_uri
+
+      rescue StandardError => e
+        $log.error(sprintf("%s: %s\n", __method__, e.message))
+        raise
+      end
+    end
+  end
+end
